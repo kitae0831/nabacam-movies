bd3ab1a0cdf7f683f4d022c3dec992e6ff71c543
kitae0831 <bkt4044@naver.com>
2023-05-29 19:22:41 +0900

    대소문자 구분 없이 검색 가능

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 const gridContainer = document.querySelector(".grid-container");
+const searchForm = document.querySelector(".search-group");
+
+const search = () => {
+  searchForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const searchText = document.getElementById("movie-search-text").value;
+
+    makeCardList(searchText);
+  });
+};
 
 // 카드의 레이아웃을 구성하여 Grid container의 자식으로 추가합니다.
 const creatCard = (payload) => {
-  const backdrop_path = payload.backdrop_path;
+  const poster_path = payload.poster_path;
   const title = payload.title;
   const overview = payload.overview;
   const vote_average = payload.vote_average;
@@ -16,7 +27,7 @@ const creatCard = (payload) => {
 
   card.className = "card-container";
   card.innerHTML = `
-  <img src="https://image.tmdb.org/t/p/w500${backdrop_path}" width="350" height="300" alt="" />
+  <img src="https://image.tmdb.org/t/p/w500${poster_path}" width="350" height="300" alt="" />
           <h2 class="card-title">${title}</h2>
           <p class="card-rating">★ ${vote_average} / 10</p>
           <p class="card-overview">${overview}</p>
@@ -47,12 +58,23 @@ const getMovieList = async () => {
   return data.results;
 };
 
-const makeCardList = async () => {
-  const movieList = await getMovieList();
+// TMDB로부터 불러온 데이터들을 가공하여 Grid template를 구성합니다.
+const makeCardList = async (searchText) => {
+  let movieList = await getMovieList();
+
+  if (searchText) {
+    gridContainer.innerHTML = "";
+    movieList = movieList.filter((movie) => {
+      const lowerCaseMovieTitle = movie.title.toLowerCase();
+      const lowerCaseSearchText = searchText.toLowerCase();
+
+      return lowerCaseMovieTitle.includes(lowerCaseSearchText);
+    });
+  }
 
   movieList.forEach((movie) =>
     creatCard({
-      backdrop_path: movie.backdrop_path,
+      poster_path: movie.poster_path,
       title: movie.title,
       overview: movie.overview,
       vote_average: movie.vote_average,
@@ -61,4 +83,10 @@ const makeCardList = async () => {
   );
 };
 
-makeCardList();
+// 함수 초기화를 함수형으로 선언합니다.
+const init = () => {
+  makeCardList();
+  search();
+};
+
+init();
